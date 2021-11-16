54e22dbce972913f21585d7ca240bb678fda1ff0
brewchetta <chett.tiller@gmail.com>
2021-11-16 16:34:37 -0500

    added controller actions

--- app/controllers/bug_items_controller.rb
+++ app/controllers/bug_items_controller.rb
@@ -1,2 +1,24 @@
 class BugItemsController < ApplicationController
+  before_action :find_bug, only: [:show, :destroy]
+
+  def index
+    @bugs = BugItems.all
+    render json: @bugs, include: category
+  end
+
+  def show
+    render json: @bug, include: category
+  end
+
+  def destroy
+    @bug.destroy
+    render json: @bug, include: category
+  end
+
+  private
+
+  def find_bug
+    @bug = BugItems.find(params[:id])
+  end
+
 end
--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,13 @@
 class CategoriesController < ApplicationController
+
+  def index
+    @categories = Category.all
+    render json: @categories, :include => bug_items
+  end
+
+  def show
+    @category = Category.find(params[:id])
+    render json: @category, :include => bug_items
+  end
+
 end
